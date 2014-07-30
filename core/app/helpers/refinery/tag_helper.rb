4954c78264cf7940d9b8bf380e96265fa9f495c1
Anita Graham <anita@joli.com.au>
2014-07-30 19:42:07 +0800

    put it back

--- core/app/helpers/refinery/tag_helper.rb
+++ core/app/helpers/refinery/tag_helper.rb
@@ -1,6 +1,5 @@
 module Refinery
   module TagHelper
-
     # Returns <img class='help' tooltip='Your Input' src='refinery/icons/information.png' />
     # Remember to wrap your block with <span class='label_with_help'></span> if you're using a label next to the help tag.
     def refinery_help_tag(title='')
@@ -14,6 +13,12 @@ module Refinery
     def refinery_icon_tag(filename, options = {})
       filename = "#{filename}.png" unless filename.split('.').many?
       image_tag "refinery/icons/#{filename}", {:width => 16, :height => 16}.merge(options)
+    end
+
+    def refinery_label_with_help(name, value=nil)
+      content_tag(:span, class: :label_with_help) do
+        label_tag( name, t(".#{name}", value)) <<  refinery_help_tag( t(".#{name}_help"))
+      end
     end
   end
 end
